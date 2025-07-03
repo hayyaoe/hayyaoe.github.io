aa476fec0795a059465212bc03d55f525d4c8fd5
hayyaoe <hayya947@gmail.com>
2025-07-03 07:32:30 +0700

    update for transfer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 function HomePage () {
   return (
     <>
-    <h1>Hello World</h1>
+      <div className="w-screen bg-space-cadet">
+        <h1 className="w-screen text-center text-2xl text-slate-100">Hello World</h1>
+      </div>
     </>
   );
 }
